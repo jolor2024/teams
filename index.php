7fa1e45abcb8a86ba1eb29d6d437571adb58ae63
Jonatan <j.lorentzson@outlook.com>
2024-10-04 20:09:42 +0200

    add style & data

--- index.php
+++ index.php
@@ -9,16 +9,24 @@ require __DIR__ . "/extrafunctions.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Startsida</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <header>
-        <h1>Startsida</h1>
+        <h1>Teams</h1>
     </header>
-    <p>Hejsan</p>
-    <?php foreach ($teams as $team) : ?>
-        <h2> <?= $team["league"] ?> </h2>
-    <?php endforeach; ?>
+
+    <div class="teams">
+        <?php foreach ($teams as $team => $value) : ?>
+            <h2> <?= $team ?> </h2>
+            <p> Liga: <?= $value["league"] ?> </p>
+            <p> Grupp: <?= $value["group"] ?> </p>
+            <p> UEFA rankning: <?= $value["uefa-coefficient-ranking"] ?> </p>
+            <p> Stad: <?= $value["city"] ?> </p>
+            <img src='<?= $value["logo"] ?>'>
+        <?php endforeach; ?>
+    </div>
 </body>
 
 </html>
