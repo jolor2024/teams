04819472b81c69ff5de896986bd2777d98742a2a
Jonatan <j.lorentzson@outlook.com>
2024-10-05 15:28:55 +0200

    fix php foreach

--- index.php
+++ index.php
@@ -16,17 +16,22 @@ require __DIR__ . "/php/head.php";
     $title = "Teams";
     require __DIR__ . "/php/header.php";
     ?>
-
-    <div class="teams">
+    <h1 class="startTitle">Fotbollsklubbar</h1>
+    <section class="teams-info">
         <?php foreach ($teams as $team => $value) : ?>
-            <h2> <?= $team ?> </h2>
-            <p> Liga: <?= $value["league"] ?> </p>
-            <p> Grupp: <?= $value["group"] ?> </p>
-            <p> UEFA rankning: <?= $value["uefa-coefficient-ranking"] ?> </p>
-            <p> Stad: <?= $value["city"] ?> </p>
-            <img src='<?= $value["logo"] ?>'>
+            <div class="team">
+                <img src='<?= $value["logo"] ?>' alt='<?= $team ?> logo'>
+                <div class="text-info">
+                    <h2><?= $team ?></h2>
+                    <p>Liga: <?= $value["league"] ?></p>
+                    <p>Grupp: <?= $value["group"] ?></p>
+                    <p>Stad: <?= $value["city"] ?></p>
+                    <p>UEFA rankning: <?= $value["uefa-coefficient-ranking"] ?></p>
+                </div>
+
+            </div>
         <?php endforeach; ?>
-    </div>
+    </section>
 
     <?php
     require __DIR__ . "/php/footer.php";
