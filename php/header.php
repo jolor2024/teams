334399e9138d1276592273e8257231ca39c599cd
Jonatan <j.lorentzson@outlook.com>
2024-10-05 19:46:18 +0200

    url fix

--- php/header.php
+++ php/header.php
@@ -1,8 +1,8 @@
 <header>
-    <img src="../teamslogo.svg" alt="logo" style="width: 250px; height:60px">
+    <img src="teamslogo.svg" alt="logo" style="width: 250px; height:60px">
     <nav>
         <ul>
-            <li><a href="/">Hem</a></li>
+            <li><a href="index.php">Hem</a></li>
             <li><a href="about.php">Om mig</a></li>
         </ul>
     </nav>
